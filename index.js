786e75023666cf0d747994447c9dcf303639c1a3
Sajjad Anwar <me@sajjad.in>
2015-12-17 21:20:52 +0530

    fix messing with geojson

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 'use strict'
-var turffc = require('turf-featurecollection');
 
-module.exports = function (feature) {
-    var fc = turffc(feature);
+module.exports = function (geojson) {
     var baseURL = "http://localhost:8111/load_object?new_layer=true&objects=";
 
-    fc.features.forEach(function (f) {
+    var featuresLength = geojson.features.length;
+    geojson.features.forEach(function (f) {
         var prefix = Object.keys(f.properties).indexOf('_osm_way_id') == -1 ? 'n' : 'w';
         var osmID = f.properties._osm_way_id || f.properties._osm_node_id;
         f.properties['_josm_url'] = baseURL + prefix + osmID;
     });
-    return fc;
+    return geojson;
 };
